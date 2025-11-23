2a9f68b651be206a90ac136c50d45a937b66479a
LangRevolution <galangprasia100@gmail.com>
2025-11-23 21:23:41 +0700

    Update MountMadyopuro-By-LangRvltn.lua

--- MountMadyopuro-By-LangRvltn.lua
+++ MountMadyopuro-By-LangRvltn.lua
@@ -1,1 +1,29 @@
-https://pastebin.com/raw/JWyqaWZi
+Name_Space : { RobloxMountFish, true 
+
+Target-Lock-Fish-Cought = Name_Fish : El Maja/ Kraken/ Legend/ Raja Ubur Ubur/ Sapu Sapu Ghoib/ MegaIjo/ Purple Kraken/ CrabKing/ Crimson/ Legendery,
+Ratio-Up = All_Rod : 0,10Second,
+Success-Up-Lock-Fish : 10KG,
+
+Name_Game : (Https://Roblox.com)
+Name_Map : (Mount Madyopuro)
+
+Features : LoadGame,
+
+Welcome-To-Script-Lang-Rvltn : true,
+
+Date-Script : {,
+
+Auto-Fishing : (On/Off) true, All_Rod_Used : mount madyopuro, true : user,
+
+Auto-Lock-Fish : true = user,
+
+Auto-Cought-Perfect-Fish : true,
+
+Skip-Fish-tilte-Last : true,
+
+Tool-Coins-User : get = true,
+
+Infinite-Coins : Last-Time-Permanent,
+
+Loadstring(game:Get('Mount-Madyopuro.get/fish/head/main')()
+AccessFully : Name_Space = Mount Madyopuro,
